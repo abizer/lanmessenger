2e150a4e186b69140199d2bbb90285cd7d710acd
Kevin Kuehler <kevin.kuehler@gmail.com>
2023-07-03 00:11:40 -0700

    At the point where i need to start refactoring

--- ui.py
+++ ui.py
@@ -1,5 +1,8 @@
 import dearpygui.dearpygui as dpg
 from enum import Enum
+from collections import OrderedDict
+import uuid
+import random
 
 class Friend:
     class Status(Enum):
@@ -10,12 +13,62 @@ class Friend:
         # Not sending discovery pings 
         OFFLINE = 3
 
-    def __init__(self, username, status=Status.ONLINE):
+    def __init__(self, username, uuid=uuid.uuid4(), status=Status.ONLINE):
         self.username = username
-        self.message_buffer = ""
         self.status = status
+        self.uuid = uuid
 
-DIMENSIONS = [800, 600]
+    def __hash__(self):
+        return hash(self.uuid)
+
+    def __eq__(self, other):
+        return self.username == other.username and self.uuid == other.uuid
+
+class Message:
+    def __init__(self, content, outgoing):
+        self.content = content
+        self.outgoing = outgoing
+
+abizer = Friend("Abizer")
+liam   = Friend("Liam")
+rachel = Friend("Rachel")
+
+friend_buffers = OrderedDict()
+friend_buffers[abizer] = []
+friend_buffers[liam] = []
+friend_buffers[rachel] = []
+
+def fill_stub_data():
+    stub_text = []
+    with open("declaration.txt") as f:
+        for line in f.readlines():
+            l = line.strip()
+            if len(l) > 0:
+                stub_text.append(l)
+
+    max_line = len(stub_text)
+
+    for friend, message_buffer in friend_buffers.items():
+        num_sent = random.randrange(10, 20)
+        num_recv = random.randrange(10, 20)
+        
+        sent_start = random.randrange(0, max_line-1)
+        recv_start = random.randrange(0, max_line-1)
+
+        outgoing = [ Message(d, True) for d in stub_text[sent_start:] ]
+        incoming = [ Message(d, False) for d in stub_text[recv_start:] ]
+        while len(outgoing) > 0 and len(incoming) > 0:
+            if random.random() > 0.5:
+                friend_buffers[friend].append(outgoing.pop())
+            else:
+                friend_buffers[friend].append(incoming.pop())
+        while len(incoming) > 0:
+                friend_buffers[friend].append(incoming.pop())
+        while len(outgoing) > 0:
+                friend_buffers[friend].append(outgoing.pop())
+
+
+DIMENSIONS = [1368, 1000]
 FONT_SCALE = 1.25
 
 def clamp(num, min_value, max_value):
@@ -28,6 +81,8 @@ def adjust_layout():
     dpg.configure_item("chatbox", width=new_chatbox_width)
     dpg.configure_item("text_scroll", height=int(DIMENSIONS[1]*0.6))
     dpg.configure_item("pals_list", width=pals_window_width)
+
+    print(DIMENSIONS[0], DIMENSIONS[1])
 
 def viewport_changed_callback(sender, data):
     DIMENSIONS[0] = data[0]
@@ -57,6 +112,8 @@ with dpg.handler_registry():
 
 DEFAULT_FONT_INDEX = 4
 MENU_FONT_INDEX    = 2
+
+fill_stub_data()
 
 fonts = {} 
 MIN_FONT_SIZE = 12
@@ -107,13 +164,31 @@ with dpg.window(label="main",
             with dpg.collapsing_header(label="LAN pals", default_open=True):
                 with dpg.group(horizontal=False):
                     def _single_selection(sender, app_data, user_data):
-                        pals = user_data
+                        pals = user_data[0]
+                        friend = user_data[1]
                         for p in pals:
                             if p != sender:
                                 dpg.set_value(p, False)
-                    pals = [ dpg.add_selectable(label=f"Pal {i}") for i in range(10) ]
-                    for p in pals:
-                        dpg.configure_item(p, callback=_single_selection, user_data=pals)
+
+                        dpg.delete_item("text_scroll", children_only=True)
+
+                        spaces = len(friend.username) - 2
+                        fromyou = spaces * ' ' + "Me:"
+                        fromthem = f'{friend.username}:'
+                        padding = 20
+                        wrap = dpg.get_item_width("text_scroll") - padding
+                        for message in friend_buffers[friend]:
+                            with dpg.group(horizontal=True, parent="text_scroll"):
+                                if message.outgoing:
+                                    dpg.add_text(default_value=fromyou, color=(53, 116, 176))
+                                else:
+                                    dpg.add_text(default_value=fromthem, color=(227, 79, 68))
+                                dpg.add_text(default_value=message.content, wrap=wrap-6)
+                        dpg.set_y_scroll('text_scroll', -1.0)
+
+                    pals = [ dpg.add_selectable(label=friend.username) for friend in friend_buffers.keys() ]
+                    for pal, friend in zip(pals, friend_buffers):
+                        dpg.configure_item(pal, callback=_single_selection, user_data=(pals, friend))
 
 dpg.bind_font(fonts[DEFAULT_FONT_SIZE])
 dpg.bind_item_font("MainMenu", fonts[MENU_FONT_SIZE])
