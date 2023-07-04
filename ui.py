46127df1c47f95597b6b26f63437b61ebb678233
Kevin Kuehler <kevin.kuehler@gmail.com>
2023-07-03 17:53:07 -0700

    formalize status buttons

--- ui.py
+++ ui.py
@@ -67,6 +67,37 @@ class UI:
                 dpg.add_text(default_value=message.content, wrap=wrap)
         self.goto_most_recent_message()
 
+    # New friend detected in the LAN, existing friend's online status changed
+    def on_friends_list_changed(self):
+        COLOR_ONLINE      = (149, 196, 124)
+        COLOR_ONLINE_FILL = (183, 224, 162)
+        COLOR_AWAY        = (237, 232, 74)
+        COLOR_AWAY_FILL   = (240, 237, 165)
+
+        dpg.delete_item(self.friends_collapsable_header, children_only=True)
+        with dpg.group(parent=self.friends_collapsable_header, horizontal=False):
+            def _friend_selection(sender, app_data, user_data):
+                # Only a single friend can be selected at a time
+                new_active_friend = user_data[0]
+                friend_items = user_data[1]
+                for item in friend_items:
+                    if item != sender:
+                        dpg.set_value(item, False)
+                self.on_selected_friend_changed(new_active_friend)
+
+            friends_list = []
+            for i, friend in enumerate(MOCK_FRIENDS.keys()):
+                with dpg.group(horizontal=True):
+                    with dpg.drawlist(width=self.current_font_size, height=self.current_font_size):
+                        center = self.current_font_size//2+1
+                        if i == 0:
+                            dpg.draw_circle((center, center), center//2, color=COLOR_ONLINE, fill=COLOR_ONLINE_FILL)
+                        else:
+                            dpg.draw_circle((center, center), center//2, color=COLOR_AWAY, fill=COLOR_AWAY_FILL)
+                    friends_list.append(dpg.add_selectable(label=friend.username))
+            for item, friend in zip(friends_list, MOCK_FRIENDS.keys()):
+                dpg.configure_item(item, callback=_friend_selection, user_data=(friend, friends_list))
+
     # Creates the settings menu
     def menu_bar(self):
         self.menu_bar = dpg.add_menu_bar(parent=self.main_window)
@@ -76,6 +107,7 @@ class UI:
             dpg.configure_item(sender, default_value=new_font_size)
             self.current_font_size = new_font_size
             dpg.bind_item_font(self.content_area, self.fonts[self.current_font_size])
+            self.on_friends_list_changed()
 
         with dpg.menu(parent=self.menu_bar, label="Settings"):
             dpg.add_input_int(label="font_size", default_value=self.current_font_size, step=1, callback=_change_font_size)
@@ -95,7 +127,7 @@ class UI:
                     # clear box and refocus
                     dpg.configure_item(input_box, default_value="")
                     dpg.focus_item(input_box)
-                
+
                 if self.active_friend is not None:
                     MOCK_FRIENDS[self.active_friend].append(Message(input, True))
                     self.on_selected_friend_changed(self.active_friend, True)
@@ -107,35 +139,17 @@ class UI:
         # Friends list
         with dpg.child_window(parent=self.content_area, width=-1):
             self.friends_collapsable_header = dpg.add_collapsing_header(label="LAN Friends", default_open=True)
-            with dpg.group(parent=self.friends_collapsable_header, horizontal=False):
-                def _friend_selection(sender, app_data, user_data):
-                    # Only a single friend can be selected at a time
-                    new_active_friend = user_data[0]
-                    friend_items = user_data[1]
-                    for item in friend_items:
-                        if item != sender:
-                            dpg.set_value(item, False)
-                    self.on_selected_friend_changed(new_active_friend)
-
-                friends_list = []
-                for friend in MOCK_FRIENDS.keys():
-                    with dpg.group(horizontal=True):
-                        with dpg.drawlist(width=self.current_font_size, height=self.current_font_size):
-                            center = self.current_font_size//2+1
-                            dpg.draw_circle((center, center), 4, color=(71, 161, 78), fill=(71, 161, 78))
-                        friends_list.append(dpg.add_selectable(label=friend.username)) 
-                for item, friend in zip(friends_list, MOCK_FRIENDS.keys()): 
-                    dpg.configure_item(item, callback=_friend_selection, user_data=(friend, friends_list))
+            self.on_friends_list_changed()
 
     def create_layout(self):
-        self.main_window = dpg.add_window(label="main_window", 
-                        width=self.dim.width, 
-                        height=self.dim.height, 
-                        no_close=True, 
-                        menubar=False, 
-                        no_collapse=True, 
-                        no_title_bar=True, 
-                        no_resize=True, 
+        self.main_window = dpg.add_window(label="main_window",
+                        width=self.dim.width,
+                        height=self.dim.height,
+                        no_close=True,
+                        menubar=False,
+                        no_collapse=True,
+                        no_title_bar=True,
+                        no_resize=True,
                         no_move=True)
         self.menu_bar()
         self.content_area()
@@ -157,10 +171,10 @@ class UI:
             dpg.set_viewport_resize_callback(self.viewport_changed_callback)
         self.register_fonts()
         self.create_layout()
-        dpg.create_viewport(title='LAN Messenger', 
-                            width=self.dim.width, 
-                            height=self.dim.height, 
-                            min_width=self.min_dim.width, 
+        dpg.create_viewport(title='LAN Messenger',
+                            width=self.dim.width,
+                            height=self.dim.height,
+                            min_width=self.min_dim.width,
                             min_height=self.min_dim.height)
         dpg.setup_dearpygui()
         dpg.show_viewport()
