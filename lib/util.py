c1e73f61c441c2d8fb0b699611c2b25b2acf3090
Kevin Kuehler <kevin.kuehler@gmail.com>
2023-07-08 15:15:41 -0700

    braindead gitignore

---
+++ lib/util.py
@@ -0,0 +1,18 @@
+from typing import Set, Union
+import netifaces
+from ipaddress import IPv4Address, IPv6Address
+
+IPAddress = Union[IPv4Address, IPv6Address]
+
+
+def get_lan_ips(v6=False) -> Set[IPAddress]:
+    ips = set()
+    family = netifaces.AF_INET6 if v6 else netifaces.AF_INET
+    for iface in netifaces.interfaces():
+        addresses = netifaces.ifaddresses(iface)
+        if family in addresses:
+            for addr in addresses[family]:
+                ip = IPv6Address(addr["addr"]) if v6 else IPv4Address(addr["addr"])
+                if ip.is_private and not ip.is_loopback and not ip.is_link_local:
+                    ips.add(ip)
+    return ips
